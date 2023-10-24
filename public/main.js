ef7113a2133cff4130dfabfc9009a00e0c67aee7
Sophia Zhang <ssz.windyskies@gmail.com>
2023-10-24 10:17:22 -0700

    nyan cat methods moved to main class
    works!

--- public/main.js
+++ public/main.js
@@ -45,58 +45,104 @@ class Main {
         }
     }
 
-    blackScreen() {
-        document.body.style.backgroundColor = "black";
-        document.getElementById("increment-button").style.display = "none";
+    setupRainbowTrail(event) {
+
+        const container = document.getElementById('rainbow-trail');
+
+        const flower = document.createElement("img");
+        flower.src = "rainbowTrail.png"; // Replace with the path to your rainbow image
+    
+        // Set the width and height of the rainbow image
+        flower.style.width = "100px"; // Adjust the width as needed
+        flower.style.height = "100px"; // Adjust the height as needed
+    
+        flower.style.position = "absolute";
+        flower.style.left = (event.pageX - 25) + "px"; // Adjust the values as needed
+        flower.style.top = (event.pageY - 25) + "px";
+        container.appendChild(flower);
+    
+        // Remove the flower element after a short duration
+        setTimeout(function() {
+            container.removeChild(flower);
+        }, 700); // Adjust the duration as needed
     }
+
+    setupNyanCat(event) {
+
+        const container1 = document.getElementById('nyan-cat');
+
+        const flower = document.createElement("img");
+        flower.src = "nyanCat.png"; // Replace with the path to your Nyan Cat image
+    
+        // Set the width and height of the Nyan Cat image
+        flower.style.width = "100px"; // Adjust the width as needed
+        flower.style.height = "100px"; // Adjust the height as needed
+    
+        flower.style.position = "absolute";
+        flower.style.left = (event.pageX - 25) + "px"; // Adjust the values as needed
+        flower.style.top = (event.pageY - 25) + "px";
+        container1.appendChild(flower);
+    
+        // Remove the Nyan Cat element after a short duration
+        setTimeout(function() {
+            container1.removeChild(flower);
+        }, 15); // Adjust the duration as needed
+    }
+
 }
 
 const mainInstance = new Main();
 
-function setupRainbowTrail(container, event) {
-    const flower = document.createElement("img");
-    flower.src = "rainbowTrail.png"; // Replace with the path to your rainbow image
+// function setupRainbowTrail(container, event) {
+//     const flower = document.createElement("img");
+//     flower.src = "rainbowTrail.png"; // Replace with the path to your rainbow image
 
-    // Set the width and height of the rainbow image
-    flower.style.width = "100px"; // Adjust the width as needed
-    flower.style.height = "100px"; // Adjust the height as needed
+//     // Set the width and height of the rainbow image
+//     flower.style.width = "100px"; // Adjust the width as needed
+//     flower.style.height = "100px"; // Adjust the height as needed
 
-    flower.style.position = "absolute";
-    flower.style.left = (event.pageX - 25) + "px"; // Adjust the values as needed
-    flower.style.top = (event.pageY - 25) + "px";
-    container.appendChild(flower);
+//     flower.style.position = "absolute";
+//     flower.style.left = (event.pageX - 25) + "px"; // Adjust the values as needed
+//     flower.style.top = (event.pageY - 25) + "px";
+//     container.appendChild(flower);
 
-    // Remove the flower element after a short duration
-    setTimeout(function() {
-        container.removeChild(flower);
-    }, 700); // Adjust the duration as needed
-}
+//     // Remove the flower element after a short duration
+//     setTimeout(function() {
+//         container.removeChild(flower);
+//     }, 700); // Adjust the duration as needed
+// }
 
-function setupNyanCat(container, event) {
-    const flower = document.createElement("img");
-    flower.src = "nyanCat.png"; // Replace with the path to your Nyan Cat image
+// function setupNyanCat(container, event) {
+//     const flower = document.createElement("img");
+//     flower.src = "nyanCat.png"; // Replace with the path to your Nyan Cat image
 
-    // Set the width and height of the Nyan Cat image
-    flower.style.width = "100px"; // Adjust the width as needed
-    flower.style.height = "100px"; // Adjust the height as needed
+//     // Set the width and height of the Nyan Cat image
+//     flower.style.width = "100px"; // Adjust the width as needed
+//     flower.style.height = "100px"; // Adjust the height as needed
 
-    flower.style.position = "absolute";
-    flower.style.left = (event.pageX - 25) + "px"; // Adjust the values as needed
-    flower.style.top = (event.pageY - 25) + "px";
-    container.appendChild(flower);
+//     flower.style.position = "absolute";
+//     flower.style.left = (event.pageX - 25) + "px"; // Adjust the values as needed
+//     flower.style.top = (event.pageY - 25) + "px";
+//     container.appendChild(flower);
 
-    // Remove the Nyan Cat element after a short duration
-    setTimeout(function() {
-        container.removeChild(flower);
-    }, 15); // Adjust the duration as needed
-}
+//     // Remove the Nyan Cat element after a short duration
+//     setTimeout(function() {
+//         container.removeChild(flower);
+//     }, 15); // Adjust the duration as needed
+// }
 
 // Event bindings outside the class
-document.addEventListener("mousemove", function(event) {
-    setupRainbowTrail(document.getElementById("rainbow-trail"), event);
-    setupNyanCat(document.getElementById("nyan-cat"), event);
-});
+// document.addEventListener("mousemove", function(event) {
+//     setupRainbowTrail(document.getElementById("rainbow-trail"), event);
+//     setupNyanCat(document.getElementById("nyan-cat"), event);
+// });
 
+document.addEventListener('mousemove', (event) => {
+    mainInstance.setupRainbowTrail(event);
+});
+document.addEventListener('mousemove', (event) => {
+    mainInstance.setupNyanCat(event);
+});
 
 document.getElementById('switch-content').addEventListener('click', () => {
     mainInstance.switchContent();
