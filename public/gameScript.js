8741f8b14f227f315f00ea6f2a9d9d94019ea83b
Sophia Zhang <ssz.windyskies@gmail.com>
2023-10-27 17:00:20 -0700

    snake game skeleton
    snake moves, can collect, score is being incremented

--- public/gameScript.js
+++ public/gameScript.js
@@ -4,23 +4,21 @@ class Game {
         this.ctx = this.canvas.getContext('2d');
         this.score = 0;
 
-        // Define the shapes array and initialize it
-        this.shapes = [];
-
-        // Initialize the snake with a single segment
         this.snake = [{ x: 100, y: 100, width: 20, height: 20 }];
+        this.snakeTail = [];
 
-        // Set the initial direction of the snake
         this.direction = 'right';
+        this.moveInterval = 5;
+        this.lastMoveTime = 0;
 
-        // Start game loop
+        this.collectibles = [];
+        // Call it initially to generate collectibles.
+        this.generateCollectibles();
+        
+        // Initialize the game
         this.update();
-
-        // Handle keyboard input
         document.addEventListener('keydown', (e) => this.handleInput(e));
 
-                // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
         const firebaseConfig = {
             apiKey: "AIzaSyBeEjuYK24GtSdcXaV1uZG-r8Bd2qFg1GY",
             authDomain: "whateverprojectsophiazhang.firebaseapp.com",
@@ -31,45 +29,36 @@ class Game {
             measurementId: "G-4ZQYQV644W"
         };
 
-
-        // Initialize Firebase
         const app = firebase.initializeApp(firebaseConfig);
-
-        // Initialize Cloud Firestore and get a reference to the service
         this.db = app.firestore(app);
     }
 
     displayScore() {
-        this.ctx.font = "24px Arial"; // Set the font size and type
-        this.ctx.fillStyle = "black"; // Set the text color
-        this.ctx.fillText("Score: " + this.score, 20, 30); // Draw the score on the canvas
-
+        this.ctx.font = "24px Arial";
+        this.ctx.fillStyle = "black";
+        this.ctx.fillText("Score: " + this.score, 20, 30);
     }
 
     submitScore(playerInitials, playerScore) {
         this.db.collection('scores').add({
-          initials: playerInitials,
-          score: playerScore,
+            initials: playerInitials,
+            score: playerScore,
         })
         .then((docRef) => {
-          console.log('Score submitted with ID: ', docRef.id);
+            console.log('Score submitted with ID: ', docRef.id);
         })
         .catch((error) => {
-          console.error('Error submitting score: ', error);
+            console.error('Error submitting score: ', error);
         });
-      }
-    
+    }
 
     gameOver() {
-        // Stop the game loop (update function)
         cancelAnimationFrame(this.animationFrameId);
 
-        // Display a game over message
         this.ctx.font = "30px Arial";
         this.ctx.fillStyle = "red";
         this.ctx.fillText("Game Over", this.canvas.width / 2 - 70, this.canvas.height / 2);
 
-        // Submit the score to Firestore
         const playerInitials = prompt('Game over! Enter your initials (3 letters):');
         if (playerInitials && playerInitials.length === 3) {
             this.submitScore(playerInitials, this.score);
@@ -78,79 +67,133 @@ class Game {
         }
     }
 
-
     redraw() {
-        // Clear the canvas
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        // Draw the snake
+    
+        // Draw collectibles
+        this.ctx.fillStyle = "red"; // Set the color you want for collectibles
+        for (const collectible of this.collectibles) {
+            this.ctx.fillRect(collectible.x, collectible.y, collectible.width, collectible.height);
+        }
+    
+        // Draw the snake's head
+        this.ctx.fillStyle = "blue"; // Change color as needed
         for (const segment of this.snake) {
-            this.ctx.fillStyle = "green";
             this.ctx.fillRect(segment.x, segment.y, segment.width, segment.height);
         }
-
+    
+        // Draw the snake's tail
+        this.ctx.fillStyle = "green"; // Change color as needed
+        for (const segment of this.snakeTail) {
+            this.ctx.fillRect(segment.x, segment.y, segment.width, segment.height);
+        }
+    
         this.displayScore();
     }
+    
 
     handleInput(e) {
         switch (e.key) {
             case 'ArrowUp':
+                if (this.direction !== 'down') {
+                    this.direction = 'up';
+                }
+                break;
             case 'ArrowDown':
+                if (this.direction !== 'up') {
+                    this.direction = 'down';
+                }
+                break;
             case 'ArrowLeft':
+                if (this.direction !== 'right') {
+                    this.direction = 'left';
+                }
+                break;
             case 'ArrowRight':
-                this.direction = e.key;
+                if (this.direction !== 'left') {
+                    this.direction = 'right';
+                }
                 break;
             default:
                 // Ignore other keys
                 break;
         }
+    }
+    
+
+    generateCollectibles() {
+        const desiredCount = 2;
+        while (this.collectibles.length < desiredCount) {
+            const randomX = Math.floor(Math.random() * this.canvas.width);
+            const randomY = Math.floor(Math.random() * this.canvas.height);
+            const collectible = { x: randomX, y: randomY, width: 20, height: 20 };
+            this.collectibles.push(collectible);
+        }
     }    
 
-    // Add a new method to move the snake
+    checkCollision() {
+        for (let i = this.collectibles.length - 1; i >= 0; i--) {
+            const collectible = this.collectibles[i];
+            if (
+                this.snake[0].x < collectible.x + collectible.width &&
+                this.snake[0].x + this.snake[0].width > collectible.x &&
+                this.snake[0].y < collectible.y + collectible.height &&
+                this.snake[0].y + this.snake[0].height > collectible.y
+            ) {
+                this.score++;
+                this.collectibles.splice(i, 1);
+                this.generateCollectibles();
+            }
+        }
+    }
+
     moveSnake() {
-        const head = { ...this.snake[0] };
+        const newHead = { ...this.snake[0] };
+    
         switch (this.direction) {
-            case 'ArrowUp':
-                head.y -= 2;
+            case 'up':
+                newHead.y -= 2;
                 break;
-            case 'ArrowDown':
-                head.y += 2;
+            case 'down':
+                newHead.y += 2;
                 break;
-            case 'ArrowLeft':
-                head.x -= 2;
+            case 'left':
+                newHead.x -= 2;
                 break;
-            case 'ArrowRight':
-                head.x += 2;
+            case 'right':
+                newHead.x += 2;
                 break;
         }
-        this.snake.unshift(head);
-        this.snake.pop();
-    }    
-
     
+        this.snake.unshift(newHead);
+    
+        while (this.snake.length > this.score + 1) {
+            this.snake.pop();
+        }
+    
+        this.snakeTail = this.snake.slice(1);
+    }
+    
+
     update() {
         this.redraw();
-
+    
         if (this.score >= 3) {
             this.gameOver();
             return;
         }
-
-        // Call moveSnake before collision detection
-        this.moveSnake();
-
-        // Your collision detection logic for the snake goes here
-
-        // Use requestAnimationFrame for smooth animations
+    
+        // Call checkCollision here before updating the snake's position
+        this.checkCollision();
+    
+        const currentTime = performance.now();
+        if (currentTime - this.lastMoveTime > this.moveInterval) {
+            this.lastMoveTime = currentTime;
+            this.moveSnake();
+        }
+    
         requestAnimationFrame(() => this.update());
     }
-    
-    
-
 }
 
-// Initialize the game
 document.myGame = new Game();
-
-// log the score once every second
-setInterval(() => console.log(document.myGame.score), 1000);
